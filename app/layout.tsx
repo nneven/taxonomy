38691e842f5769d53dccb3922be7b266cf967ba4
Bebop <nneven@usc.edu>
2024-01-10 01:40:11 -0500

    test min-h-dvh

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,7 +77,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <head />
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-dvh bg-background font-sans antialiased",
           fontSans.variable,
           fontHeading.variable
         )}
