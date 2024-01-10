d37eeaa52d203071aa0a0f373b0ded595257895a
Bebop <nneven@usc.edu>
2024-01-10 01:15:21 -0500

    default section padding

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -45,7 +45,7 @@ export default async function IndexPage() {
         className="absolute bottom-0 right-0 z-[-1] h-full w-full object-cover opacity-10"
         src="videos/abstract.mov"
       />
-      <section className="space-y-6 py-32">
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
           <div className="rounded-2xl bg-background px-4 py-2 text-sm font-medium">
             Coming Soon
