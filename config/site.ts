6fa470c7fcad42a19bb3520798d8675885f107d5
Bebop <nneven@usc.edu>
2024-01-11 16:57:18 -0500

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ import { SiteConfig } from "types"
 
 export const siteConfig: SiteConfig = {
   name: "Vantager",
-  description: "Elevating Investment Intelligence with AI Insights",
+  description: "Elevating Investment Intelligence with AI-Driven Insights",
   url: "https://vantager.co",
   ogImage: "/",
   links: {
